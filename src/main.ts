8f5b198f81b3d67a71c406fc13c4c0445d248ead
NongChen <2584533077@qq.com>
2022-12-28 21:39:28 +0800

    登录页面

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import router from "./router";
+import "element-plus/dist/index.css";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ElementPlus, { size: "small", zIndex: 3000 });
+app.use(router);
+app.mount("#app");
