cfc33e168dc9ef53b7fd7afad2075ad2b0faeaad
NongChen <2584533077@qq.com>
2023-11-11 00:08:51 +0800

    update:测试

--- src/view/canvas/screenshot.vue
+++ src/view/canvas/screenshot.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div class="item a">
+      <p>A</p>
+      <!-- <p>width:10</p> -->
+    </div>
+    <div class="item b">
+      <p>B</p>
+      <!-- <p>width:50</p> -->
+    </div>
+    <div class="item c">
+      <p>C</p>
+      <!-- <p>width:10</p> -->
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -8,5 +21,30 @@ import { ref, reactive, toRefs } from "vue";
 
 <style lang="scss" scoped>
 .container {
+  margin: 10px;
+  display: flex;
+  width: 100px;
+  height: 200px;
+  background-color: #eee;
+  color: #666;
+  text-align: center;
+  .item {
+    height: 100px;
+  }
+  .a {
+    width: 30px;
+    background-color: #ff4466;
+    flex: 0.4;
+  }
+  .b {
+    width: 30px;
+    background-color: #42b983;
+    // flex: 0;
+  }
+  .c {
+    width: 40px;
+    flex: 1.6;
+    background-color: #61dafb;
+  }
 }
 </style>
