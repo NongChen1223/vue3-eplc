89a47a29ca432575f09661c8bc1f6e4919178c53
NongChen <2584533077@qq.com>
2023-11-13 00:45:50 +0800

    update:画板

--- src/view/canvas/drawingBoard.vue
+++ src/view/canvas/drawingBoard.vue
@@ -1,32 +1,47 @@
 <template>
   <div class="container">
     <div class="main">
-      <canvas id="board" width="200" height="100"></canvas>
+      <div
+        ref="board"
+        style="border: 1px solid red; width: 200px; height: 100px"
+      ></div>
     </div>
     <div class="bottom">123123</div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { fa } from "element-plus/es/locale";
 import { ref, reactive, toRefs, onMounted } from "vue";
 const board = ref<HTMLElement | null>(null);
 const canvas = ref<HTMLCanvasElement | null>(null);
 const ctx = ref<CanvasRenderingContext2D | null>(null);
-
+const startX = ref(0);
+const startY = ref(0);
+//路径历史
+const pathHistory = ref<any[]>([]);
 onMounted(() => {
-  initBoard();
+  createBoard();
+  // mouseDown();
+  console.log("画布", board.value);
 });
-const initBoard = () => {
-  board.value = document.getElementById("board")!;
-  if (board.value) {
-    canvas.value = document.createElement("canvas");
-    ctx.value = canvas.value.getContext("2d");
-    //创建矩形
-    if (ctx.value) {
-      ctx.value.fillStyle = "black";
-      ctx.value.fillRect(0, 0, 600, 300);
-    }
-  }
+//创建画布
+const createBoard = () => {
+  canvas.value = document.createElement("canvas");
+  canvas.value.width = board.value.clientWidth;
+  canvas.value.height = board.value.clientHeight;
+  canvas.value.style.display = "block";
+  canvas.value.style.backgroundColor = "#000";
+  board.value.appendChild(canvas.value);
+  canvas.value.addEventListener("mousedown", mouseDown);
+};
+//鼠标按下事件
+const mouseDown = (e) => {
+  console.log("鼠标移动", e);
+  ctx.value = canvas.value?.getContext("2d");
+  //创建路径
+  ctx.value?.beginPath(e.offsetX, e.offsetY);
+  ctx.value.stroke();
 };
 </script>
 
